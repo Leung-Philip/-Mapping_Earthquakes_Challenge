30f9602b7c20390fbe74f65ab3d60df8fdb92dcd
Philip Leung <92767386+Leung-Philip@users.noreply.github.com>
2022-01-22 10:09:14 -0600

    update marker colors to match legend

--- static/js/challenge_logic.js
+++ static/js/challenge_logic.js
@@ -73,7 +73,10 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 
   // This function determines the color of the marker based on the magnitude of the earthquake.
   function getColor(magnitude) {
-    if (magnitude > 5) {
+    if (magnitude > 6) {
+      return "#9E1C00";
+    }
+    else if (magnitude > 5) {
       return "#ea2c2c";
     }
     else if (magnitude > 4) {
@@ -139,13 +142,13 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
   // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
     function getColor(magnitude) {
     if (magnitude > 6) {
-      return "#ea2c2c";
+      return "#9E1C00";
     }
     else if (magnitude > 5) {
-      return "#ea822c";
+      return "#ea2c2c";
     }
     else {
-      return "#98ee00";
+      return "#ea822c";
   }}
   
   // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
@@ -182,17 +185,18 @@ let legend = L.control({
 });
 
 // Then add all the details for the legend
-legend.onAdd = function() {
+legend.onAdd = function(map) {
   let div = L.DomUtil.create("div", "info legend");
 
-  const magnitudes = [0, 1, 2, 3, 4, 5];
+  const magnitudes = [0, 1, 2, 3, 4, 5, 6];
   const colors = [
     "#98ee00",
     "#d4ee00",
     "#eecc00",
     "#ee9c00",
     "#ea822c",
-    "#ea2c2c"
+    "#ea2c2c",
+    "#9E1C00"
   ];
 
 // Looping through our intervals to generate a label with a colored square for each interval.
@@ -207,7 +211,6 @@ legend.onAdd = function() {
 
   // Finally, we our legend to the map.
   legend.addTo(map);
-
 
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
   d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(tectonicData) {
